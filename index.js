a3b2e0c411be92567e48e429641147002652f52c
FrankZou21 <frankzou2000@hotmail.com>
2019-11-22 11:03:06 -0800

    testing

--- index.js
+++ index.js
@@ -5,3 +5,5 @@ AppRegistry.registerComponent('ViroSample', () => App);
 
 // The below line is necessary for use with the TestBed App
 AppRegistry.registerComponent('ViroSample', () => App);
+
+//HELLO
