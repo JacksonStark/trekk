2a8fc0dc03dc754240debf6fd3fcbe11d81abe93
Jackson Stark <jacksonstark77@hotmail.com>
2019-11-24 17:59:35 -0800

    implement scene visibility onClick of Star

--- js/HelloWorldSceneAR.js
+++ js/HelloWorldSceneAR.js
@@ -14,14 +14,11 @@ import {
   ViroBox,
   ViroImage,
   ViroNode,
-  ViroVideo
+  ViroVideo,
+  Viro3DObject,
+  ViroAmbientLight
 } from 'react-viro';
-import { makeEmptyAggregatedTestResult } from '@jest/test-result';
 
-// const markers = {
-//   first: false,
-//   second: true
-// }
 
 const userMarkers = [
   {
@@ -51,7 +48,7 @@ let cameraPos;
 
 export default function HelloWorldSceneAR(props) {
   const [text, setText] = useState("Initializing AR...")
-  const [show, setShow] = useState(false)
+  // const [show, setShow] = useState(false)
   const [visibility, setVisibility] = useState({})
 
   const onInitialized = (state, reason) => {
@@ -66,13 +63,11 @@ export default function HelloWorldSceneAR(props) {
 
     console.log('Marker--Camera Distance', Math.abs(anchor.position[2] - cameraPos[2]))
 
-    Math.abs(anchor.position[0] - cameraPos[0]) < 0.8 &&
-    Math.abs(anchor.position[1] - cameraPos[1]) < 0.8 &&
-    Math.abs(anchor.position[2] - cameraPos[2]) < 0.8
-    ? setVisibility((prev) => ({...prev, [currentMarker]: true}))
-    : setVisibility((prev) => ({...prev, [currentMarker]: false}))
-    // ? setVisibility(true)
-    // : setVisibility(false)
+  //   Math.abs(anchor.position[0] - cameraPos[0]) < 0.8 &&
+  //   Math.abs(anchor.position[1] - cameraPos[1]) < 0.8 &&
+  //   Math.abs(anchor.position[2] - cameraPos[2]) < 0.8
+  //   ? setVisibility((prev) => ({...prev, [currentMarker]: true}))
+  //   : setVisibility((prev) => ({...prev, [currentMarker]: false}))
 
   }
 
@@ -80,29 +75,22 @@ export default function HelloWorldSceneAR(props) {
     cameraPos = cam.position
   }
 
-  const onClick = (e) => {
-    show ? setShow(false) : setShow(true)
-  }
+  const onClick = (anchor, currentMarker) => {
+    console.log(visibility[currentMarker])
 
-  // ViroARTrackingTargets.createTargets({
-  //   "qr" : {
-  //     source : require('./res/qr.png'),
-  //     orientation : "Up",
-  //     physicalWidth : 0.2, // real world width in meters
-  //   },
-  //   "rubber-duck" : {
-  //     source : require('./res/rubber-duck.jpg'),
-  //     orientation : "Up",
-  //     physicalWidth : 0.2, // real world width in meters
-  //   }
-  // });
+    visibility[currentMarker]
+    ? setVisibility((prev) => ({...prev, [currentMarker]: false}))
+    : setVisibility((prev) => ({...prev, [currentMarker]: true}))
+
+    // show ? setShow(false) : setShow(true)
+  }
 
   const imageMarkers = userMarkers.map((marker, index) => {
     let currentMarker = 'marker' + index
 
     let targetObj = {}
 
-    targetObj[index] = {
+    targetObj[currentMarker] = {
     source : {uri: marker.markerImage},
     orientation : "Up",
     physicalWidth : 0.2, // real world width in meters
@@ -110,55 +98,34 @@ export default function HelloWorldSceneAR(props) {
 
     ViroARTrackingTargets.createTargets(targetObj);
 
-    visibility ? null : setVisibility((prev) => ({...prev, [currentMarker]: true}))
+    visibility[currentMarker] === undefined ? setVisibility((prev) => ({...prev, [currentMarker]: false})) : null
 
     return(
 
-      <ViroARImageMarker target={index} onAnchorUpdated={(anchor) => onAnchorUpdated(anchor, currentMarker)} visible={visibility[currentMarker]}>
+      <ViroARImageMarker key={currentMarker} target={currentMarker} onAnchorUpdated={((anchor) => onAnchorUpdated(anchor, currentMarker))}>
 
-      {/* <ViroImage rotation={[-90, 0, 0]} position={[0,0,0]} scale={[0.3, 0.3, 0.3]} source={{uri: marker.spawnedImage}} onClick={(e) => onClick(e)} resizeMode={'scaleToFill'} /> */}
+        <ViroAmbientLight color='#ffffff' />
 
-      <ViroNode position={[0,0,0]} rotation={[-90, 0, 0]} dragType="FixedToWorld" onDrag={()=>{}}>
-        {marker.spawnedDescription && (
-          <ViroText text={marker.spawnedDescription} scale={[0.3, 0.3, 0.3]} position={[0, 0, -0.1]} style={styles.viroText} />
-        )}
+        <Viro3DObject rotation={[-90, 0, 0]} source={require('./res/object_star_anim.vrx')} position={[0, 0, 0.15]} scale={[0.05, 0.05, 0.05]} type="VRX" onClick={(anchor) => onClick(anchor, currentMarker)} />
 
-        {marker.spawnedImage && (
-          <ViroImage position={[0,0,0]} scale={[0.3, 0.3, 0.3]} source={{uri: marker.spawnedImage}} onClick={(e) => onClick(e)} resizeMode={'scaleToFill'} />
-        )}
+        <ViroNode position={[0,0,0]} rotation={[-90, 0, 0]} dragType="FixedToWorld" onDrag={()=>{}} visible={visibility[currentMarker]}>
+          {marker.spawnedDescription && (
+            <ViroText text={marker.spawnedDescription} scale={[0.3, 0.3, 0.3]} position={[0, 0, -0.1]} style={styles.viroText} />
+          )}
 
-        {marker.spawnedVideo && ( 
-          <ViroVideo height={0.8} width={1.4} source={{uri: marker.spawnedVideo}} loop={true} position={[0,0,0]} material={[marker.markerImage]} scale={[0.2, 0.2, 0.2]} volume={1} />
-        )}
-      </ViroNode>
+          {marker.spawnedImage && (
+            <ViroImage position={[0,0,0]} scale={[0.3, 0.3, 0.3]} source={{uri: marker.spawnedImage}} resizeMode={'ScaleToFill'} />
+          )}
+
+          {marker.spawnedVideo && ( 
+            <ViroVideo height={0.8} width={1.4} source={{uri: marker.spawnedVideo}} loop={true} position={[0,0,0]} scale={[0.2, 0.2, 0.2]} volume={1} />
+          )}
+        </ViroNode>
 
       </ViroARImageMarker> 
     )
 
   })
-  
-  // return (
-  // <ViroARScene onTrackingUpdated={onInitialized} onCameraTransformUpdate={(e) => onCameraTransformUpdate(e)}>
-  //   {/* VIRO TEXTBOX (Width, Height) */}
-  //     <ViroARImageMarker target={"rubber-duck"} onAnchorUpdated={(e) => onAnchorUpdated(e)} visible={visibility}>
-
-  //         <ViroNode position={[0,0,0]} rotation={[-90, 0, 0]} dragType="FixedToWorld" onDrag={()=>{}}>
-  //         <ViroImage height={0.2} width={0.2} source={require('./res/Beach.jpg')} onClick={(e) => onClick(e)}/>
-  //           <ViroText height={2} width={2} text={text} scale={[0.5, 0.5, 0.5]} position={[0, 2, -1]} visible={show} style={styles.viroText}/>
-  //         </ViroNode>
-
-  //     </ViroARImageMarker>
-
-  //     <ViroARImageMarker target={"qr"} onAnchorUpdated={(e) => onAnchorUpdated(e)}>
-
-  //         <ViroNode position={[0,0,0]} rotation={[-90, 0, 0]} dragType="FixedToWorld" onDrag={()=>{}} visible={visibility} >
-  //           <ViroImage height={0.2} width={0.2} source={require('./res/Guadalupe.jpg')} onClick={(e) => onClick(e)} />
-  //           <ViroText height={1} width={2} text={text} scale={[0.5, 0.5, 0.5]} position={[0, 0.8, -1]} visible={show} style={styles.viroText}/>
-  //         </ViroNode>
-
-  //     </ViroARImageMarker>
-  //   </ViroARScene>
-  // );
 
   return (
     <ViroARScene onTrackingUpdated={onInitialized} onCameraTransformUpdate={(e) => onCameraTransformUpdate(e)}>
@@ -167,16 +134,6 @@ export default function HelloWorldSceneAR(props) {
   )
 
 }
-
-
-
-
-
-
-
-
-
-
 
 
 var styles = StyleSheet.create({
