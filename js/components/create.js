2a4cf9f4f358f7d9789689797cab7e146a8244dd
adamleseur <adamleseur@gmail.com>
2019-11-26 13:43:29 -0800

    Adds display for image of point.

--- js/components/create.js
+++ js/components/create.js
@@ -6,13 +6,68 @@ import {
   StyleSheet,
   TextInput,
   Button,
-  ImageBackground
+  ImageBackground,
+  Image
  } from 'react-native';
 
 export default function Create({transition}) {
 
   const [name, setName] = useState('')
-  const [description, setDescription = useState('')]
+
+  const markersData = [
+    {
+      id: 1,
+      name: 'Tits',
+      markerImage: 'https://img.ev.mu/images/regions/91/600x400/91.jpg',
+      spawnedDescription: "This is Guadalupe. TITS",
+      spawnedImage: null,
+      spawnedVideo: null
+    },
+    {
+      id: 2,
+      name: 'Magnemite',
+      markerImage: 'https://img.ev.mu/images/attractions/1336/600x400/1851.jpg',
+      spawnedDescription: null,
+      spawnedImage: "http://static.pokemonpets.com/images/monsters-images-800-800/81-Magnemite.png",
+      spawnedVideo: null
+    },
+    {
+      id: 3,
+      name: 'Fireworks',
+      markerImage: 'https://www.kitchentreaty.com/wp-content/uploads/2017/02/how-to-make-heart-shaped-pizzas-featured-660x430.jpg',
+      spawnedDescription: null,
+      spawnedImage: null,
+      spawnedVideo: 'https://v.ftcdn.net/02/29/37/75/700_F_229377542_Y4dQ5kuAj6FPafId0XIdZ9jKcDQykYF8_ST.mp4'
+    }
+  ]
+
+  const markers = markersData.map((marker) => {
+    return (
+      <View>
+        <Text>
+          Marker Name: {marker.name}
+        </Text>
+        <Text>
+          Marker Image:
+        </Text>
+      <Image
+          style={{width: 50, height: 50}}
+          source={{uri: marker.markerImage}}
+        />
+        {marker.spawnedDescription && (
+          <Text>Displays text.</Text>
+        )}
+
+        {marker.spawnedImage && (
+          <Text>Displays an image.</Text>
+        )}
+
+        {marker.spawnedVideo && ( 
+          <Text>Displays a video.</Text>
+        )}
+        </View>
+    )
+  })
 
   return (
     <ImageBackground style={localStyles.outer}>
@@ -32,7 +87,7 @@ export default function Create({transition}) {
             style={localStyles.text}
           />
 
-          <Text>
+          {/* <Text>
             Description:
           </Text>
           
@@ -40,15 +95,13 @@ export default function Create({transition}) {
             placeholder = "Description of Trekk"
             onChangeText= {(n) => {setDescription(n)}}
             style={localStyles.text}
-          />
+          /> */}
 
           <Text>
             Current Markers:
           </Text>
 
-          <Text>
-            There are currently no markers in this Trekk.
-          </Text>
+          {markers}
 
           <Button
           title = "Add New Marker"
